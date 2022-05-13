9d52e417adfa0394fadd29858b77568a65ef7903
jjpaya <jjpaya@users.noreply.github.com>
2022-05-13 12:46:15 +0200

    django: fix balance calculation signal error

--- backend/bike_rider/apps/payments/signals.py
+++ backend/bike_rider/apps/payments/signals.py
@@ -5,12 +5,14 @@ from django.db.models import Sum
 from .models import Payment, Charge
 from ..users.models import User
 
+
 @receiver(post_save, sender=Payment)
 def update_user_balance_s_payment(sender, instance, *args, **kwargs):
     if not (instance and instance.confirmed):
         return
 
     update_user_balance(sender, instance)
+
 
 @receiver(post_save, sender=Charge)
 def update_user_balance_s_charge(sender, instance, *args, **kwargs):
@@ -19,9 +21,12 @@ def update_user_balance_s_charge(sender, instance, *args, **kwargs):
 
     update_user_balance(sender, instance)
 
+
 def update_user_balance(sender, instance):
-    sum_payments = Payment.objects.aggregate(Sum('total_eur_cent'))['total_eur_cent__sum']
-    sum_charges = Charge.objects.aggregate(Sum('total_eur_cent'))['total_eur_cent__sum']
+    sum_payments = Payment.objects.filter(user=instance.user, confirmed=True).aggregate(
+        Sum('total_eur_cent'))['total_eur_cent__sum']
+    sum_charges = Charge.objects.filter(user=instance.user).aggregate(
+        Sum('total_eur_cent'))['total_eur_cent__sum']
 
     sum_payments = sum_payments if sum_payments is not None else 0
     sum_charges = sum_charges if sum_charges is not None else 0
